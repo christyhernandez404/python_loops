1da8f11f61d8217e2e30dfb699be0d33869130ea
christyhernandez404 <christyhernandez404@gmail.com>
2024-06-04 16:06:38 -0400

    removed comments

--- question4_task1.py
+++ question4_task1.py
@@ -1,4 +1,3 @@
-# Create a game where a player has a list of items. They have to guess which item in the list was selected. Use random.choice() to select the item and take the user's guess via input. Provide feedback on whether they guessed correctly or not keep them playing until they guess correctly
 import random
 
 items = ['candlestick', 'knife', 'cat toy', 'newspaper', 'rope', 'air fryer']
